dc7a44876da422f13cb4d3a18814088d4ed60dc5
Michael Ploughman <michael@michaelhploughman.com>
2019-11-13 12:03:02 -0700

    console.log dog pictures

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function getDogImage(numImages) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numImages}`)
+    .then(response => response.json())
+    .then(responseJson =>
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson.message);
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let numImages = $('#numImages').val();
+    $('#numImages').val('3');
+    getDogImage(numImages);
+  });
+}
+
+$(function () {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
